cf1fb359724a7025899016599ada0939593dffac
Filip Lindahl <filip.lindahl@skf.com>
2021-04-14 16:49:27 +0200

    very nice

--- src/server.js
+++ src/server.js
@@ -2,11 +2,12 @@ const http = require("http");
 
 function createServer(config = {}) {
   const { buildNumber = "n/a" } = config;
-
+  let fastCode = "";
+  var bad_Code = "hullo";
   const server = http.createServer((req, res) => {
     console.log(`Received request ${req.method.toUpperCase()} ${req.url}`);
     res.writeHead(200, "OK");
-    res.write(Buffer.from(`Hello World! Build number ${buildNumber}\n`));
+    res.write(Buffer.from(`${bad_Code} Build number ${buildNumber}\n`));
     res.end();
   });
 
